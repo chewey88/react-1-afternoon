9452bcff0ee75a3a530738175019eb16ae5b60cc
Christopher Sovine <chewey88@gmail.com>
2020-03-25 21:45:50 -0600

    React1-project

--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -4,21 +4,52 @@ class FilterObject extends Component {
     constructor(){
         super();
         this.state = {
-            employess: [
+            gemstones: [
+                {
+                    name: 'Emerald',
+                    typicalColor: 'green',
+                    hardness: 8,
+                },
+                {
+                    name: 'Saphire',
+                    typicalColor: 'blue',
+                    chemicalComp: 'Al2O3',
+                },
+                {
+                    name: 'Ruby',
+                    typicalColor: 'red',
+                    density: 4,
+                }
 
             ],
             userInput: '',
-            filteredEmployees: []
+            filterGemstones: []
         }
+    }
+    handleChange(value) {
+        this.setState({ userInput: value })
+    }
+
+    filterGemstones(prop){
+        let gemstones = this.state.gemstones;
+        let filteredGemstones = [];
+
+    for ( let i = 0; i < gemstones.length; i++ ) {
+      if ( gemstones[i].hasOwnProperty(prop) ) {
+        filteredGemstones.push(gemstones[i]);
+      }
+    }
+
+    this.setState({ filteredGemstones: filteredGemstones })
     }
     render() {
         return(
             <div className='puzzleBox filterObjectPB'>
                 <h4> Filter Object </h4>
-                <span className='resultsBox'> </span>
-                <input className='inputLine' />
-                <button className='confirmationButton'> Filter </button> 
-                <span className='resultsBox filterObjectRB'></span>
+                <span className='puzzleText'> Original: { JSON.stringify(this.state.gemstones, null, 10)} </span>
+                <input className='inputLine' onChange={ (e) => this.handleChange(e.target.value)} />
+                <button className='confirmationButton'onClick={ () => this.filterGemstones(this.state.userInput)}> Filter </button> 
+                <span className='resultsBox filterObjectRB'>Filtered: { JSON.stringify(this.state.filteredGemstones, null, 10) }</span>
             </div> 
         )
     }
--- src/components/Topics/FilterString.js
+++ src/components/Topics/FilterString.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react'
 
 class FilterString extends Component {
-    render() {
-        return(
-            <p> FilterString Component</p>
-        )
+
+    constructor() {
+      super();
+  
+      this.state = {
+        martialWeapons: ['Katana', 'Long Sword', 'Shurikan', 'BlowGun', 'Flail'],
+        userInput: '',
+        filteredMartialWeapons: []
+      };
     }
-}
+  
+    handleChange(val) {
+      this.setState({ userInput: val });
+    }
+  
+    filteredMartialWeapons(userInput) {
+      let martialWeapons = this.state.martialWeapons;
+      let filteredMartialWeapons = [];
+  
+      for ( let i = 0; i < martialWeapons.length; i++ ) {
+        if ( martialWeapons[i].includes(userInput) ) {
+          filteredMartialWeapons.push(martialWeapons[i]);
+        }
+      }
+  
+      this.setState({ filteredMartialWeapons: filteredMartialWeapons });
+    }
+  
+    render() {
+      return (
+        <div className="puzzleBox filterStringPB">
+          <h4> Filter String </h4>
+          <span className="puzzleText"> martialWeapons: { JSON.stringify(this.state.martialWeapons, null, 10) } </span>
+          <input className="inputLine" onChange={ (e) => this.handleChange(e.target.value) }/>
+          <button className="confirmationButton" onClick={ () => this.filteredMartialWeapons(this.state.userInput) }> Filter </button>
+          <span className="resultsBox filterStringRB"> Filtered MartialWeapons: { JSON.stringify(this.state.filteredMartialWeapons, null, 10) } </span>
+        </div>
+      )
+    }
+  }
 
 export default FilterString;
